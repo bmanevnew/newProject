c879a05db51408200c7ca0df3cb7da184077eacd
Boris Manev <boris.manev.a44@learn.telerikacademy.com>
2023-06-08 18:44:05 +0300

    commit2

--- dbScripts/createScript.sql
+++ dbScripts/createScript.sql
@@ -1,24 +1,6 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.5.18-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
-
-
--- Dumping database structure for deltagroup
-CREATE DATABASE IF NOT EXISTS `deltagroup` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
-USE `deltagroup`;
 
 -- Dumping structure for table deltagroup.jobs
-CREATE TABLE IF NOT EXISTS `jobs`
+CREATE TABLE IF NOT EXISTS deltagroup.jobs
 (
     `job_id`      int(11)       NOT NULL AUTO_INCREMENT,
     `name`        varchar(100)  NOT NULL,
@@ -33,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `jobs`
 -- Data exporting was unselected.
 
 -- Dumping structure for table deltagroup.news
-CREATE TABLE IF NOT EXISTS `news`
+CREATE TABLE IF NOT EXISTS deltagroup.news
 (
     `name`        varchar(200) NOT NULL,
     `description` varchar(3000)         DEFAULT NULL,
@@ -50,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `news`
 -- Data exporting was unselected.
 
 -- Dumping structure for table deltagroup.users
-CREATE TABLE IF NOT EXISTS `users`
+CREATE TABLE IF NOT EXISTS deltagroup.users
 (
     `username`  varchar(32) NOT NULL,
     `password`  varchar(32) NOT NULL,
@@ -64,8 +46,3 @@ CREATE TABLE IF NOT EXISTS `users`
   COLLATE = latin1_swedish_ci;
 
 -- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS = IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES = IFNULL(@OLD_SQL_NOTES, 1) */;
--- src/main/java/com/example/newproject/Main.java
+++ src/main/java/com/example/newproject/Main.java
@@ -3,8 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewProjectApp {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(NewProjectApp.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
